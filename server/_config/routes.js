0fcd9d2a040a803cc171a2cd6ae6335157b6d47f
Abraham Bueno <abueno@localhost.localdomain>
2018-05-16 22:30:35 -0500

    Completed requirements for the day. Will continue the rest as the resources become available.

--- server/_config/routes.js
+++ server/_config/routes.js
@@ -62,26 +62,20 @@ const authenticate = passport.authenticate('local', passportOptions);
 const protected = passport.authenticate('jwt', passportOptions);
 
 // helpers
-function makeToken(user) {
-  const timestamp = new Date().getTime();
-  const payload = {
-    sub: user._id,
-    iat: timestamp,
-    username: user.username,
-  };
-  const options = {
-    expiresIn: '24h',
-  };
+// function makeToken(user) {
+//   const timestamp = new Date().getTime();
+//   const payload = {
+//     sub: user._id,
+//     iat: timestamp,
+//     username: user.username,
+//   };
+//   const options = {
+//     expiresIn: '24h',
+//   };
 
-  return jwt.sign(payload, secret, options);
-}
-// function authenticate(req, res, next) {
-//   if (req.session && req.session.username) {
-//     next();
-//   } else {
-//     res.status(401).send('You shall not pass!!');
-//   }
+//   return jwt.sign(payload, secret);
 // }
+
 
 // routes
 module.exports = function (server) {
@@ -100,11 +94,13 @@ module.exports = function (server) {
 
 
 
-  server.get('/api/users', protected, (req, res) => {
+  server.get('/api/users/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    console.log('res:', res)
     User.find()
       .select('username')
       .then(users => {
-        res.json(users);
+        // res.json(users);
+        res.send(users)
       })
       .catch(err => {
         res.status(500).json(err);
@@ -122,48 +118,69 @@ module.exports = function (server) {
 
 
   server.get('/api/logout', (req, res) => {
-    if (req.session) {
-      console.log('Current Session logout, session:', req.session);
-      req.session.destroy(function (err) {
-        if (err) {
-          res.send("error");
-        } else {
-          res.send('Goodbye');
-        }
-      });
-    };
+    req.logout();
+    res.status(200).json("You are logged out")
+    // res.status(200).json({ token: makeToken(req.user), user: req.user });
+
+    // if (req.session) {
+    //   console.log('Current Session logout, session:', req.session);
+    //   req.session.destroy(function (err) {
+    //     if (err) {
+    //       res.send("error");
+    //     } else {
+    //       res.send('Goodbye');
+    //     }
+    //   });
+    // };
   });
 
-  server.get('/api/', (req, res) => {
-
+  server.get('/api/', protected, (req, res) => {
+    // server.get('/', function(req, res) {
+    // res.send({ api: 'up and running' });
+    // });
     // if (req.session && req.session.username) {
-    //   res.send(`Welcome back ${req.session.username}`)
+      // res.send(`Welcome back ${req.session.username}`)
     // } else {
     //   res.send("Who are you, really? Don't lie to me!")
     // }
+    console.log('This is the data inside api:',req);
+    res.send(`Welcome back ${req.user.username}`)
+    // User
+    // .find()
+    // .select('username')
+    // .then(user => {
+    //   res.send(`Welcome back ${req.user.username}`)
+    //   res.send(user)
+    // })
+    // .catch(err => {
+    //   res.status(401).json("You shall not pass, for this api")
+    // })
   });
-//<<TESTED
+  //<<TESTED
   server.post('/api/login', authenticate, (req, res) => {
-    res.status(200).json({token: makeToken(req.user), user: req.user});
-  //   const { username, password } = req.body;
-  //   const passBody = req.body.password;
+    // var token = jwt.sign(user, config.secret);
+    console.log('req',req.user)
+    // return the information including token as JSON
+    // res.json({ success: true, token: 'JWT ' + token });
+    res.status(200).json({ token: makeToken(req.user), user: req.user });
 
-  //   const userInfo = { username, password };
-  //   User.findOne({ username })
-  //     .then(user => {
-  //       if (user) {
-  //         user.isPasswordValid(password).then(isValid => {
-  //           if (isValid) {
-  //             req.session.username = user.username;
-  //             res.send("Have a cookie");
-
-  //           } else {
-  //             res.status(401).send('Invalid user-name')
-  //           }
-  //         });
-  //       } else {
-  //         res.status(404).send("Invalid PASSWORD")
-  //       }
-  //     }).catch(err => res.send(err))
   });
+
+  function makeToken(user) {
+    const timestamp = new Date().getTime();
+    const payload = {
+      sub: user._id,
+      iat: timestamp,
+      username: user.username,
+    };
+    const options = {
+      expiresIn: '24h',
+    };
+  
+    let ttoken = jwt.sign(payload, secret, options);
+    console.log('This is the token', ttoken);
+    return ttoken;
+  }
+
+
 };
